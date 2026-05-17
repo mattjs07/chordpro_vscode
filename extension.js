28a44ddd0a81ff756e47a4c3b9982866ecd9b879
mattjs07 <mattis.gjlbert@gmail.com>
2026-05-17 15:44:32 +0200

    Fix: fullscreen exit uses evenEditorWidths for consistent 50/50 split on restore

--- extension.js
+++ extension.js
@@ -2660,7 +2660,7 @@ function activate(context) {
             } else if (msg.command === 'enterFullscreen') {
                 vscode.commands.executeCommand('workbench.action.maximizeEditorHideSidebar');
             } else if (msg.command === 'exitFullscreen') {
-                vscode.commands.executeCommand('workbench.action.minimizeOtherEditors');
+                vscode.commands.executeCommand('workbench.action.evenEditorWidths');
                 vscode.commands.executeCommand('workbench.action.toggleSidebarVisibility');
             }
         });
